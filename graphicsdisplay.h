960b1503cf45546d005e519c44a02412a2d26514
Kapil Bilimoria <k2bilimo@gmail.com>
2018-04-04 17:23:37 -0400

    added score and turnstate

--- graphicsdisplay.h
+++ graphicsdisplay.h
@@ -8,15 +8,20 @@ class GraphicsDisplay{
 	int colourPositive = Xwindow::Brown;
 	int colourNegative = Xwindow::Orange;
 	int flipRow(int row);
+	float blackScore = 0;
+	float whiteScore = 0;
+	
 	public:
 	//ctor
 	GraphicsDisplay();
+	//dtor
 	~GraphicsDisplay();
 	//constants
-	const int gridsize = 8;
-	const int cellsize = windowsize/gridsize; 
+	const int gridsize = 10;
+	const int cellsize = windowsize/gridsize; //62.5 pixels for each cell.
 	const int yPadding = cellsize/2;
 	const int xPadding = cellsize/4;
+	const std::string font ="-adobe-helvetica-medium-o-normal--24-240-75-75-p-130-iso8859-14"; // font for score and turn are subject to change.
 	//methods
 	int findRectangleColour(int col, int row);
 	int findPieceColour(char piece);
@@ -24,5 +29,8 @@ class GraphicsDisplay{
 	void updateMove(char piece,const std::string &start,const std::string &end);
 	void clearBoard();
 	void defaultDisplay();
+	void updateScore(bool winnerIsWhite, bool isStalemate);
+	void updateTurn(bool white);
+	void clearTop(bool forTurn);
 };
 #endif
